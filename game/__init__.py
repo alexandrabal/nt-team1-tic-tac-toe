07abf6f40cdebd2301dc56f3ddb38af6e109ec40
Alexandra <65454440+alexandrabal@users.noreply.github.com>
2021-02-28 22:15:50 +0000

    many_rounds

--- game/__init__.py
+++ game/__init__.py
@@ -2,9 +2,7 @@ from .rounds import check_player_choice,play_rounds
 
 
 def start_play():
-    number_of_games = check_player_choice()
-    play_rounds(number_of_games)
-
+    play_rounds(check_player_choice())
 
 
 
